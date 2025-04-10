941e4229fdf08dc86da190a8a1e550fddfd5ad1c
Paurush M Taunk <paurushtaunk@gmail.com>
2025-04-11 00:55:27 +0530

    Adding Navigation Mechanism

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreditCardFormComponent } from "./credit-card-form/credit-card-form.component";
+import { CardGridComponent } from "./card-grid/card-grid.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'credit-store-app';
+
+  
 }
